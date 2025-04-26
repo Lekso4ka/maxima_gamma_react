fdd5d5e2cdab647bb643af1a556145618f883345
Leksa <admin@petiteweb.ru>
2025-04-26 11:41:49 +0300

    upd configs

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
 				use: ["style-loader", "css-loader"]
 			},
 			{
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+			{
 				test: /\.(png|jpg|gif|woff(2)?|eot|ttf|otf)$/,
 				type: "asset/resource"
 			},
